527b7b789dd963fbc1196133932731b03f3da42b
waigoma <waichi0302@gmail.com>
2022-03-03 16:26:17 +0900

    update

--- main/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/waigoma/GenshinCalender
-
-go 1.17
---
+++ src/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{})
+	})
+
+	err := router.Run()
+	if err != nil {
+		return
+	}
+}
