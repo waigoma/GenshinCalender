ce1252ea029b697b58d58d332f06ecf83bad6071
waigoma <waichi0302@gmail.com>
2022-03-04 23:14:13 +0900

    update

--- src/genshin/resin/CalculateResin.go
+++ src/genshin/resin/CalculateResin.go
@@ -1,5 +1,11 @@
 package resin
 
+import (
+	"github.com/waigoma/GenshinCalender/src/genshin/character"
+	"github.com/waigoma/GenshinCalender/src/genshin/region"
+	"github.com/waigoma/GenshinCalender/src/genshin/talent"
+)
+
 type Mode int
 
 const (
@@ -29,4 +35,22 @@ func CalculateRegenTime(reqResin int, mode Mode) float64 {
 	default:
 		return -1
 	}
+}
+
+func CalculateTotalResin(characterStatList []character.Stats) int {
+	var totalResin int
+	for _, characterStat := range characterStatList {
+		// 必要な天賦本数を計算
+		var needTalent int
+		for _, books := range characterStat.Talent {
+			needTalent += talent.CalculateTalentBooks(map[string]int{books.Type: books.Count})
+		}
+
+		// 周回数を計算
+		grindTime := region.CalculateGrind(needTalent)
+
+		// 必要な樹脂数
+		totalResin += CalculateNeedResin(grindTime)
+	}
+	return totalResin
 }
