22a8142cbaca5cc1f0ca03e7a99a6aed51ea7215
waigoma <waichi0302@gmail.com>
2022-03-04 23:14:21 +0900

    update result

--- src/web/ResultHandler.go
+++ src/web/ResultHandler.go
@@ -3,10 +3,29 @@ package web
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/waigoma/GenshinCalender/src/genshin/character"
-	"github.com/waigoma/GenshinCalender/src/genshin/region"
 	"github.com/waigoma/GenshinCalender/src/genshin/resin"
 	"github.com/waigoma/GenshinCalender/src/genshin/talent"
+	"github.com/waigoma/GenshinCalender/src/useful"
 	"net/http"
+)
+
+type Type int
+
+type SelectForm struct {
+	CharacterName string
+	TalentForms   []TalentForm
+}
+
+type TalentForm struct {
+	Type Type
+	From string
+	To   string
+}
+
+const (
+	NormalAttack Type = iota
+	SkillAttack
+	ULTAttack
 )
 
 func RegisterResultHandler(router *gin.Engine) {
@@ -15,6 +34,7 @@ func RegisterResultHandler(router *gin.Engine) {
 }
 
 func resultGetHandle(ctx *gin.Context) {
+	ctx.Redirect(http.StatusFound, "/")
 	ctx.HTML(
 		http.StatusOK,
 		"result.html",
@@ -24,27 +44,11 @@ func resultGetHandle(ctx *gin.Context) {
 }
 
 func resultPostHandle(ctx *gin.Context) {
-	characterArray := ctx.PostFormArray("character")
-	fromArray := ctx.PostFormArray("from")
-	toArray := ctx.PostFormArray("to")
+	// フォームから受け取ったデータを CharacterStat に変換
+	characterStatList := getResult(initSelectForm(ctx))
 
-	characterStatList := getResult(characterArray, fromArray, toArray)
-
-	var totalResin int
-
-	for _, characterStat := range characterStatList {
-		// 必要な天賦本数を計算
-		var needTalent int
-		for _, books := range characterStat.Talent {
-			needTalent += talent.CalculateTalentBooks(map[string]int{books.Type: books.Count})
-		}
-
-		// 周回数を計算
-		grindTime := region.CalculateGrind(needTalent)
-
-		// 必要な樹脂数
-		totalResin += resin.CalculateNeedResin(grindTime)
-	}
+	// 必要樹脂数
+	totalResin := resin.CalculateTotalResin(characterStatList)
 
 	// 回復時間 (分)
 	totalTime := resin.CalculateRegenTime(totalResin, resin.ModeMinute)
@@ -59,16 +63,87 @@ func resultPostHandle(ctx *gin.Context) {
 		})
 }
 
-func getResult(characterArray []string, fromArray []string, toArray []string) []character.Stats {
+func initSelectForm(ctx *gin.Context) []SelectForm {
+	// 必要なデータを Context から取得
+	characterArray := ctx.PostFormArray("character")
+	NormalAttackArray := ctx.PostFormArray("normalAttack")
+	SkillAttackArray := ctx.PostFormArray("skillAttack")
+	ULTAttackArray := ctx.PostFormArray("ultAttack")
+
+	var fromArray [][]string
+	var toArray [][]string
+
+	// post された情報取得
+	for idx, tmp := range []string{"from", "to"} {
+		for _, num := range []string{"1", "2", "3"} {
+			if idx == 0 {
+				fromArray = append(fromArray, ctx.PostFormArray(tmp+num))
+			} else if idx == 1 {
+				toArray = append(toArray, ctx.PostFormArray(tmp+num))
+			}
+		}
+	}
+
+	// SelectForm struct にプロットしていく
+	var selectForms []SelectForm
+
+	for idx, characterName := range characterArray {
+		var selectForm SelectForm
+
+		selectForm.CharacterName = characterName
+
+		if useful.Contains(NormalAttackArray, characterName) {
+			selectForm.TalentForms = append(selectForm.TalentForms, TalentForm{
+				Type: NormalAttack,
+				From: fromArray[0][idx],
+				To:   toArray[0][idx],
+			})
+		}
+
+		if useful.Contains(SkillAttackArray, characterName) {
+			selectForm.TalentForms = append(selectForm.TalentForms, TalentForm{
+				Type: SkillAttack,
+				From: fromArray[1][idx],
+				To:   toArray[1][idx],
+			})
+		}
+
+		if useful.Contains(ULTAttackArray, characterName) {
+			selectForm.TalentForms = append(selectForm.TalentForms, TalentForm{
+				Type: ULTAttack,
+				From: fromArray[2][idx],
+				To:   toArray[2][idx],
+			})
+		}
+
+		selectForms = append(selectForms, selectForm)
+	}
+
+	return selectForms
+}
+
+func getResult(selectForms []SelectForm) []character.Stats {
 	var characterStatList []character.Stats
 
 	// 選択したキャラクターをすべてカウント
-	for idx, characterName := range characterArray {
+	for _, selectForm := range selectForms {
 		// 選択したキャラクター
-		chara := character.GetCharacter(characterName)
+		chara := character.GetCharacter(selectForm.CharacterName)
 
 		// 天賦本の数
-		talentBookCount := talent.CountTalentBooks(fromArray[idx], toArray[idx])
+		talentBookCount := make(map[string]int)
+
+		for _, talentForm := range selectForm.TalentForms {
+			bookCounts := talent.CountTalentBooks(talentForm.From, talentForm.To)
+
+			for key, value := range bookCounts {
+				if v, ok := talentBookCount[key]; ok {
+					talentBookCount[key] = v + value
+				} else {
+					talentBookCount[key] = value
+				}
+			}
+		}
 
 		// 天賦本を取得
 		talentBook := talent.GetTalentBook(chara.TalentBook)
