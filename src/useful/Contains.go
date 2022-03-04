3b401e417498ef8d7b17e3f215c9971a6a04592c
waigoma <waichi0302@gmail.com>
2022-03-04 23:14:05 +0900

    update

--- src/useful/Contains.go
+++ src/useful/Contains.go
@@ -1,6 +1,6 @@
 package useful
 
-func contains(baseStr []string, matchStr string) bool {
+func Contains(baseStr []string, matchStr string) bool {
 	for _, base := range baseStr {
 		if base == matchStr {
 			return true
